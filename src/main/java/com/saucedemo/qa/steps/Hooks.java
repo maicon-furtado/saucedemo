6920edf77a209c6d8abc02b208cb812d47ee984c
Maicon Furtado <maicon_furtado@hotmail.com>
2023-09-10 17:07:33 +0100

    Add: extent reports

--- src/main/java/com/saucedemo/qa/steps/Hooks.java
+++ src/main/java/com/saucedemo/qa/steps/Hooks.java
@@ -4,11 +4,13 @@ import static com.saucedemo.qa.steps.utils.Environment.getHubUrl;
 
 import com.google.inject.Inject;
 import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
 import java.net.MalformedURLException;
 import java.net.URL;
 import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
@@ -30,8 +32,12 @@ public class Hooks {
 
   @After
   public void tearDown() {
-    world.getScenario().attach(world.getDriver().getScreenshotAs(OutputType.BYTES),
-        "image/png", "Final Screenshot");
     world.getDriver().quit();
+  }
+
+  @AfterStep
+  public void takeScreenshot(){
+    final byte [] screenshot = ((TakesScreenshot) world.getDriver()).getScreenshotAs(OutputType.BYTES);
+            world.getScenario().attach(screenshot, "image/png", world.getScenario().getName());
   }
 }
